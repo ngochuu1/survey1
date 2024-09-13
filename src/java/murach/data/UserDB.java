19a76355b246c81c8fca691d1df156bf5853fbb8
hango <hango@acer3>
2024-09-13 23:18:58 +0700

    first commit

---
+++ src/java/murach/data/UserDB.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package murach.data;
+
+import murach.business.User;
+
+/**
+ *
+ * @author hango
+ */
+public class UserDB {
+    public static long insert(User user){
+        return 0;
+    }
+}
